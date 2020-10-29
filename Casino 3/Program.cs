c58b4cea0d89f8de12594a69992b7f699e59c2b1
kenx00x <kenx00x@gmail.com>
2020-10-29 10:56:51 +0100

    Casino 3

---
+++ Casino 3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Casino_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random willekeurig = new Random();
+            Console.Write("Welk getal denk je dat de computer gooide tussen 1 en 6: ");
+            int gebruikergetal = int.Parse(Console.ReadLine());
+            if (gebruikergetal == willekeurig.Next(1, 7))
+            {
+                Console.Write("Welk getal denk je dat de computer gooide tussen 1 en 6: ");
+                gebruikergetal = int.Parse(Console.ReadLine());
+                if (gebruikergetal == willekeurig.Next(1, 7))
+                {
+                    Console.Write("Welk getal denk je dat de computer gooide tussen 1 en 6: ");
+                    gebruikergetal = int.Parse(Console.ReadLine());
+                    if (gebruikergetal == willekeurig.Next(1, 7))
+                    {
+                        Console.WriteLine("proficiat");
+                    }
+                    else
+                    {
+                        Console.WriteLine("you lose");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("you lose");
+                }
+            }
+            else
+            {
+                Console.WriteLine("you lose");
+            }
+        }
+    }
+}
