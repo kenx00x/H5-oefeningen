199ddb513e098318099cc2049b1f1cd5191da38f
kenx00x <kenx00x@gmail.com>
2020-10-29 12:00:09 +0100

    Schrikkeljaar

---
+++ Schrikkeljaar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Schrikkeljaar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Geef jaar: ");
+            int jaar = int.Parse(Console.ReadLine());
+            if (jaar%4==0)
+            {
+                Console.WriteLine($"{jaar} is een schrikkeljaar");
+            }
+            else
+            {
+                Console.WriteLine($"{jaar} is geen schrikkeljaar");
+            }
+        }
+    }
+}
