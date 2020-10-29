186118a2fbc0588cc428254e18d4f21b1f3fbfa3
kenx00x <kenx00x@gmail.com>
2020-10-29 10:51:43 +0100

    casino oefening

---
+++ Casino/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Casino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random willekeurig = new Random();
+            Console.Write("Welk getal denk je dat de computer gooide tussen 1 en 6: ");
+            int gebruikergetal = int.Parse(Console.ReadLine());
+            if (gebruikergetal == willekeurig.Next(1,7))
+            {
+                Console.WriteLine("proficiat");
+            }
+            else
+            {
+                Console.WriteLine("you lose");
+            }
+        }
+    }
+}
