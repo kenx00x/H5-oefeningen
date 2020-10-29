0e0b951fb6b00c6bf06a31db21cbbebf1e13a6de
kenx00x <kenx00x@gmail.com>
2020-10-29 11:33:00 +0100

    Schaakstuk

---
+++ Schaakstuk/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace Schaakstuk
+{
+    class Program
+    {
+        enum schaakstukken
+        {
+            [Description("witte koning")]
+            wittekoning = 0,
+            [Description("zwarte koning")]
+            zwartekoning = 1,
+            [Description("witte pion")]
+            wittepion = 2,
+            [Description("zwarte pion")]
+            zwartepion = 3,
+            [Description("wit paard")]
+            witpaard = 4,
+            [Description("zwart paard")]
+            zwartpaard = 5,
+            [Description("witte dame")]
+            wittedame = 6,
+            [Description("zwarte dame")]
+            zwartedame = 7,
+            [Description("witte loper")]
+            witteloper = 8,
+            [Description("zwarte loper")]
+            zwarteloper = 9,
+            [Description("witte toren")]
+            wittetoren = 10,
+            [Description("zwarte toren")]
+            zwartetoren = 11
+        }
+        static void Main(string[] args)
+        {
+            Random willekeurig = new Random();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"{((schaakstukken)willekeurig.Next(0, 12)).ToDescription()}");
+            }
+        }
+    }
+    internal static class Extensions
+    {
+        public static string ToDescription(this Enum value)
+        {
+            FieldInfo field = value.GetType().GetField(value.ToString());
+            DescriptionAttribute attribute = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) as DescriptionAttribute;
+            return attribute == null ? value.ToString() : attribute.Description;
+        }
+    }
+}
