29c10df1530328baed13dfc6c4f0cc8e06461406
kenx00x <kenx00x@gmail.com>
2020-10-29 13:02:43 +0100

    einde hoofdstuk 5

---
+++ Enum bij BMI/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Enum_bij_BMI
+{
+    class Program
+    {
+        enum gewichichtKlassen
+        {
+            ondergewicht,
+            normaal,
+            overgewicht,
+            zwaarlijvig,
+            ernstigZwaarlijvig
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Geef je gewicht in kg: ");
+            double gewicht = double.Parse(Console.ReadLine());
+            Console.Write("Geef je lengte in meter: ");
+            double lengte = double.Parse(Console.ReadLine());
+            double BMI = Math.Round((gewicht / (lengte * lengte)), 2);
+            if (BMI < 18.5)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Je BMI is {BMI}");
+                Console.WriteLine(gewichichtKlassen.ondergewicht);
+            }
+            else if (BMI < 25)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Je BMI is {BMI}");
+                Console.WriteLine($"{gewichichtKlassen.normaal} gewicht");
+            }
+            else if (BMI < 30)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine($"Je BMI is {BMI}");
+                Console.WriteLine($"{gewichichtKlassen.overgewicht}. Je loopt niet echt een risico, maar je mag niet dikker worden.");
+            }
+            else if (BMI < 40)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Je BMI is {BMI}");
+                Console.WriteLine($"{gewichichtKlassen.zwaarlijvig}. Verhoogde kans op allerlei aandoeningen zoals diabetes, hartaandoeningen en rugklachten. Je zou 5 tot 10 kg moeten vermageren.");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine($"Je BMI is {BMI}");
+                Console.WriteLine($"{gewichichtKlassen.ernstigZwaarlijvig}. Je moet dringend vermageren want je gezondheid is in gevaar (of je hebt je lengte of gewicht in verkeerde eenheid ingevoerd).");
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- Kleurcode weerstand naar ohm, met enum/Program.cs
+++ Kleurcode weerstand naar ohm, met enum/Program.cs
@@ -1,114 +1,131 @@
 ﻿using System;
 
-namespace Kleurcode_weerstand_naar_ohm
+namespace Kleurcode_weerstand_naar_ohm__met_enum
 {
     class Program
     {
+        enum kleurcodes
+        { 
+            zwart,
+            bruin,
+            rood,
+            oranje,
+            geel,
+            groen,
+            blauw,
+            violet,
+            grijs,
+            wit
+        }
         static void Main(string[] args)
         {
             Console.Write("Geef ring1: ");
-            string ring1 = Console.ReadLine();
-            int ring1waarde =0;
+            kleurcodes ring1;
+            Enum.TryParse(Console.ReadLine(), out ring1);
+            int ring1waarde = 0;
             switch (ring1)
             {
-                case "zwart":
+                case kleurcodes.zwart:
                     ring1waarde = 0;
                     break;
-                case "bruin":
+                case kleurcodes.bruin:
                     ring1waarde = 10;
                     break;
-                case "rood":
+                case kleurcodes.rood:
                     ring1waarde = 20;
                     break;
-                case "oranje":
+                case kleurcodes.oranje:
                     ring1waarde = 30;
                     break;
-                case "geel":
+                case kleurcodes.geel:
                     ring1waarde = 40;
                     break;
-                case "groen":
+                case kleurcodes.groen:
                     ring1waarde = 50;
                     break;
-                case "blauw":
+                case kleurcodes.blauw:
                     ring1waarde = 60;
                     break;
-                case "violet":
+                case kleurcodes.violet:
                     ring1waarde = 70;
                     break;
-                case "grijs":
+                case kleurcodes.grijs:
                     ring1waarde = 80;
                     break;
-                case "wit":
+                case kleurcodes.wit:
                     ring1waarde = 90;
                     break;
             }
             Console.Write("Geef ring2: ");
-            string ring2 = Console.ReadLine();
+            kleurcodes ring2;
+            Enum.TryParse(Console.ReadLine(), out ring2);
             int ring2waarde = 0;
             switch (ring2)
             {
-                case "zwart":
+                case kleurcodes.zwart:
                     ring2waarde = 0;
                     break;
-                case "bruin":
+                case kleurcodes.bruin:
                     ring2waarde = 1;
                     break;
-                case "rood":
+                case kleurcodes.rood:
                     ring2waarde = 2;
                     break;
-                case "oranje":
+                case kleurcodes.oranje:
                     ring2waarde = 3;
                     break;
-                case "geel":
+                case kleurcodes.geel:
                     ring2waarde = 4;
                     break;
-                case "groen":
+                case kleurcodes.groen:
                     ring2waarde = 5;
                     break;
-                case "blauw":
+                case kleurcodes.blauw:
                     ring2waarde = 6;
                     break;
-                case "violet":
+                case kleurcodes.violet:
                     ring2waarde = 7;
                     break;
-                case "grijs":
+                case kleurcodes.grijs:
                     ring2waarde = 8;
                     break;
-                case "wit":
+                case kleurcodes.wit:
                     ring2waarde = 9;
                     break;
             }
             Console.Write("Geef ring3: ");
-            string ring3 = Console.ReadLine();
+            kleurcodes ring3;
+            Enum.TryParse(Console.ReadLine(), out ring3);
             int ring3waarde = 0;
             switch (ring3)
             {
-                case "zwart":
+                case kleurcodes.zwart:
                     ring3waarde = 1;
                     break;
-                case "bruin":
+                case kleurcodes.bruin:
                     ring3waarde = 10;
                     break;
-                case "rood":
+                case kleurcodes.rood:
                     ring3waarde = 100;
                     break;
-                case "oranje":
+                case kleurcodes.oranje:
                     ring3waarde = 1000;
                     break;
-                case "geel":
+                case kleurcodes.geel:
                     ring3waarde = 10000;
                     break;
-                case "groen":
+                case kleurcodes.groen:
                     ring3waarde = 100000;
                     break;
-                case "blauw":
+                case kleurcodes.blauw:
                     ring3waarde = 1000000;
                     break;
-                case "violet":
+                case kleurcodes.violet:
                     ring3waarde = 10000000;
                     break;
             }
-            Console.WriteLine($"Deze weerstand heeft een waarde van {(ring1waarde+ring2waarde)*ring3waarde}");
+            Console.WriteLine($"Deze weerstand heeft een waarde van {(ring1waarde + ring2waarde) * ring3waarde}");
+
         }
     }
 }
